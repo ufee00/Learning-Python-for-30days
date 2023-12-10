0e1b92714d5cfd3d72d0336019d9e523272fefd2
ufee00 <ibrahimumarfaruk2000@gmail.com>
2023-12-10 19:38:36 +0100

    .

---
+++ Day_12_Modules/main.py
@@ -0,0 +1,8 @@
+# main.py file
+from mymodule import generate_full_name as fullname, sum_of_two as add, diff_of_two as diff
+
+print(fullname('Umar', 'Faruk', 'Ibrahim'))
+
+print(add(4, 2))
+
+print(diff(8, 3))
---
+++ Day_12_Modules/mymodule.py
@@ -0,0 +1,8 @@
+def generate_full_name(firstname, middlename, lastname):
+    return firstname + ' ' + middlename + ' ' + lastname
+
+def sum_of_two(num1, num2):
+    return num1 + num2
+
+def diff_of_two(num1, num2):
+    return num1 - num2
